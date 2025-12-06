0651cef11bfc7bed6da595ff8df1371a1899af6f
Leokasuki <thiellennard09@gmail.com>
2025-12-07 00:03:19 +0100

    Container hobbies hinzugefügt

--- about_leokasuki.js
+++ about_leokasuki.js
@@ -1,3 +1,12 @@
-function zeigeNachricht() {
-    alert("Hallo! Das kommt aus der externen JS-Datei.");
-}
+document.querySelectorAll('.hobby .title').forEach(item => {
+    item.addEventListener('click', () => {
+        const details = item.nextElementSibling;
+        if(details.style.display === 'block'){
+            details.style.display = 'none';
+            item.textContent = item.textContent.replace('-', '+');
+        } else {
+            details.style.display = 'block';
+            item.textContent = item.textContent.replace('+', '-');
+        }
+    });
+});
