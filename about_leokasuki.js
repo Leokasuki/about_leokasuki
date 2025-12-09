831226b9c16d884cc90b83ebd80a6eb2d3eab4ac
Leokasuki <thiellennard09@gmail.com>
2025-12-09 05:43:10 +0100

    Added taskbar and tux with gun icon

--- about_leokasuki.js
+++ about_leokasuki.js
@@ -19,10 +19,7 @@ const imageList = [
     "https://media1.tenor.com/m/XuVii4d1wrQAAAAd/go-white-boy-go-nge.gif",
     "https://media1.tenor.com/m/LqdgG1SRGf0AAAAd/cirno-hatred.gif",
     "https://media1.tenor.com/m/9gosAbRjs7YAAAAd/bocchi-boc.gif",
-    "https://media.discordapp.net/attachments/941399611707363399/1204138149068935198/1707099874355.gif?ex=69371f0a&is=6935cd8a&hm=00a69bd3ab7cce7f5d858783fd11445638d79b4cd47c323da02d4fbe3b92cd5c&",
-    "https://cdn.discordapp.com/attachments/1343250561172111360/1346781657642045511/1334210480730291844.gif?ex=693747e9&is=6935f669&hm=b07618d78cd4f1dfb66310169b844666c83df56655e4798352d6e5472b0ddeb6&",
     "https://cdn.discordapp.com/emojis/770707949684391939.gif?v=1",
-    "https://media.discordapp.net/attachments/795421457454202900/806993847355834418/image0-3.gif?ex=69373d96&is=6935ec16&hm=87126d885eb4eb9562ce60080fd6b612a6c413893c8c11e30d64597df764b1be&"
 ];
 
 const container = document.getElementById("scrollContent");
